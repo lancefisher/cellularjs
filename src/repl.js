9b1eda9aadd4f920255b451dd4fc272b031629f3
Lance Fisher <lance@submittable.com>
2017-06-04 02:45:58 -0600

    Improve REPL
    Any 'sheet.' input will be eval-ed.  Output is colored with chalk.
    
    Try:
    ```
    sheet.getCell('A1').text = 123
    sheet.getCell('B1').text = 456
    sheet.getCell('C1').text = '=A1+B1'
    sheet.calculate()
    ```
    
    You should see:
    ```
    A1: 123 => 123
    B1: 456 => 456
    C1: =A1+B1 => 579
    ```

--- src/repl.js
+++ src/repl.js
@@ -1,22 +1,39 @@
 /* eslint-env node */
-/* eslint-disable no-console */
 import repl from 'repl';
-//import { tokenize } from './tokenizer';
-import CellularParser from './CellularParser';
+import chalk from 'chalk';
+import Sheet from './Sheet';
 
-// See: https://nodejs.org/api/repl.html
+const log = console.log; // eslint-disable-line no-console
+const sheet = new Sheet();
+
+function printSheet() {
+  const lines = [];
+  sheet.cells.forEach(cell => lines.push(
+    `${chalk.cyan(cell.id)}: ${chalk.yellow(cell.text)} => ${chalk.green(cell.value)}`,
+  ));
+  return lines.join('\n');
+}
 
 function interpret(cmd) {
-  //const tokens = tokenize(cmd);
-  //return tokens;
-  const parser = new CellularParser(cmd);
-  const expression = parser.parseExpression();
-  return expression;
+  // const parser = new CellularParser(cmd);
+  // const expression = parser.parseExpression();
+  // return expression;
+  if (cmd.indexOf('sheet.') === 0) {
+    eval(cmd); // eslint-disable-line no-eval
+  }
+
+  return printSheet();
 }
 
 function evaluate(cmd, context, filename, callback) {
   callback(null, interpret(cmd));
 }
 
-console.log('Starting cellular REPL... (press ctrl-c to quit)');
-repl.start({ prompt: '> ', eval: evaluate });
+function writer(output) {
+  return output;
+}
+
+// See: https://nodejs.org/api/repl.html
+log('Starting cellular REPL... (press ctrl-c to quit)');
+repl.start({ prompt: '> ', eval: evaluate, writer });
+
